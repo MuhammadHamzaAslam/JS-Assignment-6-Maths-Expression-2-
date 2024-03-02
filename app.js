4fed6a442232f6563cb7f16c5049220016eef6d6
MuhammadHamzaAslam <hamzaaslam19087@gmail.com>
2024-03-02 22:10:18 +0500

    commit

---
+++ app.js
@@ -0,0 +1,49 @@
+var a = 10;
+document.write("<h1>The value of a is: " + a + "</h1>" );
+
+document.write("<h1>.....................</h1>")
+
+document.write("<h1>The Value of ++a is: "+ ++a + "</h1>")
+document.write("<h1>Now the value of a is:" + a + "</h1>"+"<br>")
+
+document.write("<h1>The Value of a++ is: "+ a++ + "</h1>")
+document.write("<h1>Now the value of a is:" + a + "</h1>" +"<br>")
+
+document.write("<h1>The Value of --a is: "+ --a + "</h1>")
+document.write("<h1>Now the value of a is:" + a + "</h1>" +"<br>")
+
+document.write("<h1>The Value of a-- is: "+ a-- + "</h1>")
+document.write("<h1>Now the value of a is:" + a + "</h1>" +"<br>")
+
+document.write("<h1 id='h1'>Task 2 </h1>");
+var a = 2 , b = 1
+            //   2   +  0 + 1   +  1
+var result = --a - --b + ++b + b--
+console.log(a);
+console.log(b);
+console.log(result); 
+document.write("<h1>a is:"+ a +"</h1>")
+document.write("<h1>b is:"+ b +"</h1>")
+document.write("<h1>result is:"+ result +"</h1>")
+
+var userName = prompt ("Enter Your Name")
+alert("Welcome" + " " +userName + "!")
+
+
+document.write("<h1 id='h1'>Task 3 </h1>");
+
+
+var table = prompt("Enter a number: Which multiplication table do you want?", "5");
+var time = 1;
+var result = table * time;
+document.write("<h1>" + table + " x " + time + " = " + result + "</h1>");
+document.write("<h1>" + table + " x " + ++time + " = " + result + "</h1>");
+document.write("<h1>" + table + " x " + ++time + " = " + result + "</h1>");
+document.write("<h1>" + table + " x " + ++time + " = " + result + "</h1>");
+document.write("<h1>" + table + " x " + ++time + " = " + result + "</h1>");
+document.write("<h1>" + table + " x " + ++time + " = " + result + "</h1>");
+document.write("<h1>" + table + " x " + ++time + " = " + result + "</h1>");
+document.write("<h1>" + table + " x " + ++time + " = " + result + "</h1>");
+document.write("<h1>" + table + " x " + ++time + " = " + result + "</h1>");
+document.write("<h1>" + table + " x " + ++time + " = " + result + "</h1>");
+
